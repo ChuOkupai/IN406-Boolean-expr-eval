54e2afd6543b2beefe128698230e89b51d717119
Adrien Soursou <26015179+ChuOkupai@users.noreply.github.com>
2019-04-28 21:26:55 +0200

    rip

--- eval.c
+++ eval.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 // Affiche l'erreur et quitte le programme
-#define exit_erreur() { perror("erreur"); exit(EXIT_FAILURE); }
+#define EXIT_ERREUR() { perror("erreur"); exit(EXIT_FAILURE); }
 
 // Affiche une erreur en cas d'expression incorrecte
-#define exit_invalide() { fprintf(stderr, "expression incorrecte\n"); exit(EXIT_FAILURE); }
+#define EXIT_INVALIDE() { fprintf(stderr, "expression incorrecte\n"); exit(EXIT_FAILURE); }
 
 enum type
 {
@@ -63,7 +63,7 @@ liste_token	new_liste_token(enum valeur v)
 {
 	liste_token l = (liste_token)malloc(sizeof(struct token));
 	if (! l)
-		exit_erreur();
+		EXIT_ERREUR();
 	l->type = get_type(v);
 	l->valeur = v;
 	l->suivant = NULL;
@@ -112,17 +112,16 @@ liste_token	string_to_token(const char *s)
 			s++;
 			continue;
 		}
-		else
-			exit_invalide();
+		else EXIT_INVALIDE(); // non reconnu
 		if (! l)
 		{
 			l = new_liste_token(v);
-			c = l;
+			c = l; // pointeur sur l'élément actuel
 		}
 		else
 		{
 			c->suivant = new_liste_token(v);
-			c = c->suivant;
+			c = c->suivant; // pointeur sur l'élément actuel
 		}
 		s++;
 	}
@@ -134,7 +133,7 @@ arbre_token	new_arbre_token(enum valeur v)
 {
 	arbre_token at = (arbre_token)malloc(sizeof(struct arbre));
 	if (! at)
-		exit_erreur();
+		EXIT_ERREUR();
 	at->type = get_type(v);
 	at->valeur = v;
 	at->gauche = NULL;
@@ -152,10 +151,65 @@ void	destroy_arbre_token(arbre_token at)
 	free(at);
 }
 
+void printList(liste_token l, char *name);
+
 arbre_token	liste_token_to_arbre_token(liste_token l)
 {
 	if (! l)
 		return NULL;
+	liste_token c = NULL, o = NULL, p;
+	
+	while (l)
+	{
+		p = l;
+		l = l->suivant;
+		if (p->type == CONSTANTE)
+		{
+			p->suivant = c;
+			c = p;
+		}
+		else if (p->type == OPERATEUR || p->valeur == GAUCHE)
+		{
+			p->suivant = o;
+			o = p;
+		}
+		else // (p->valeur == DROITE)
+			free(p);
+	}
+	while (c || o)
+	{
+		printList(c, "c");
+		printList(o, "o");
+		printList(l, "l");
+		if (! o) // une constante restante
+		{
+			p = l;
+			l = c;
+			c = c->suivant;
+			l->suivant = p;
+		}
+		else if (o->valeur == GAUCHE)
+		{
+			p = o->suivant;
+			free(o);
+			o = p;
+		}
+		else
+		{
+			p = l;
+			l = o;
+			o = o->suivant;
+			l->suivant = p;
+			p = l;
+			l = c;
+			c = c->suivant;
+			l->suivant = p;
+		}
+	}
+	printList(c, "c");
+	printList(o, "o");
+	printList(l, "l");
+	destroy_liste_token(l);
 	return NULL;
 }
 
@@ -206,6 +260,18 @@ const char*	getString(enum valeur v)
 	return ")";
 }
 
+void printList(liste_token l, char *name)
+{
+	if (! l) return;
+	printf("%s = ", name);
+	while(l)
+	{
+		printf("%s", getString(l->valeur));
+		l = l->suivant;
+	}
+	putchar('\n');
+}
+
 // Parcours prefixe (debug)
 void	prefixe(arbre_token at, unsigned int p)
 {
@@ -226,16 +292,15 @@ void	test_expression(const char *s)
 	liste_token l = string_to_token(s);
 	arbre_token at = liste_token_to_arbre_token(l);
 	prefixe(at, 0);
-	printf((arbre_to_int(at)) ? "VRAI" : "FAUX");
-	putchar('\n');
+	//printf((arbre_to_int(at)) ? "VRAI" : "FAUX");
+	//putchar('\n');
 	destroy_arbre_token(at);
-	destroy_liste_token(l);
 }
 
 int	main(int argc, char **argv)
 {
-	if (argc < 2)
-		exit_invalide();
+	if (argc != 2)
+		EXIT_INVALIDE();
 	test_expression(argv[1]);
 	return 0;
 }
