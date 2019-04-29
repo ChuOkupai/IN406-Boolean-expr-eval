e8b3449b020e6ff212d0e5f52d7f25e6d18f00ea
Adrien Soursou <26015179+ChuOkupai@users.noreply.github.com>
2019-04-29 21:56:03 +0200

    début fonction est_valide() (ne gère pas les parenthèses)

--- eval.c
+++ eval.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Affiche l'erreur et quitte le programme
-#define EXIT_ERREUR() { perror("erreur"); exit(EXIT_FAILURE); }
-
 // Affiche une erreur en cas d'expression incorrecte
-#define EXIT_INVALIDE() { fprintf(stderr, "expression incorrecte\n"); exit(EXIT_FAILURE); }
+#define EXIT_INVALIDE() { puts("expression incorrecte"); exit(EXIT_FAILURE); }
+
+// Ajoute un token à la file et déplace le pointeur à la fin
+#define ENQUEUE(QUEUE, TYPE, VALEUR) ({ QUEUE->suivant = new_liste_token(TYPE, VALEUR); QUEUE = QUEUE->suivant; })
 
 enum type
 {
@@ -51,20 +51,12 @@ struct arbre
 	arbre_token	droite;
 };
 
-enum type	get_type(enum valeur v)
-{
-	if (v == FAUX || v == VRAI)
-		return CONSTANTE;
-	return (v == GAUCHE || v == DROITE) ? PARENTHESE : OPERATEUR;
-}
-
 // Alloue la mémoire à un token dans la liste
-liste_token	new_liste_token(enum valeur v)
+liste_token	new_liste_token(enum type t, enum valeur v)
 {
 	liste_token l = (liste_token)malloc(sizeof(struct token));
-	if (! l)
-		EXIT_ERREUR();
-	l->type = get_type(v);
+	if (! l) exit(EXIT_FAILURE);
+	l->type = t;
 	l->valeur = v;
 	l->suivant = NULL;
 	return l;
@@ -79,63 +71,13 @@ void	destroy_liste_token(liste_token l)
 	free(l);
 }
 
-// Transforme une chaîne de charactères en liste de token
-liste_token	string_to_token(const char *s)
-{
-	liste_token l = NULL, c = NULL;
-	enum valeur v;
-	while (*s)
-	{
-		if (*s == '0')		v = FAUX;
-		else if (*s == '1')	v = VRAI;
-		else if (*s == '+')	v = OU;
-		else if (*s == '.')	v = ET;
-		else if (*s == '(')	v = GAUCHE;
-		else if (*s == ')')	v = DROITE;
-		else if (s[0] == '=' && s[1] && s[1] == '>')
-		{
-			v = IMPLICATION;
-			s++;
-		}
-		else if (s[0] == '<' && s[1] && s[1] == '=' && s[2] && s[2] == '>')
-		{
-			v = EQUIVALENCE;
-			s += 2;
-		}
-		else if (s[0] == 'N' && s[1] && s[1] == 'O' && s[2] && s[2] == 'N')
-		{
-			v = NON;
-			s += 2;
-		}
-		else if (*s == ' ')
-		{
-			s++;
-			continue;
-		}
-		else EXIT_INVALIDE(); // non reconnu
-		if (! l)
-		{
-			l = new_liste_token(v);
-			c = l; // pointeur sur l'élément actuel
-		}
-		else
-		{
-			c->suivant = new_liste_token(v);
-			c = c->suivant; // pointeur sur l'élément actuel
-		}
-		s++;
-	}
-	return l;
-}
-
 // Alloue la mémoire à un token dans l'arbre
-arbre_token	new_arbre_token(enum valeur v)
+arbre_token	new_arbre_token(liste_token l)
 {
 	arbre_token at = (arbre_token)malloc(sizeof(struct arbre));
-	if (! at)
-		EXIT_ERREUR();
-	at->type = get_type(v);
-	at->valeur = v;
+	if (! at) exit(EXIT_FAILURE);
+	at->type = l->type;
+	at->valeur = l->valeur;
 	at->gauche = NULL;
 	at->droite = NULL;
 	return at;
@@ -151,8 +93,108 @@ void	destroy_arbre_token(arbre_token at)
 	free(at);
 }
 
-void printList(liste_token l, char *name);
+// Conversion d'un token en chaîne de charactères (DEBUG)
+const char*	toString(enum valeur v)
+{
+	if (v == FAUX)
+		return "0";
+	else if (v == VRAI)
+		return "1";
+	else if (v == NON)
+		return "NON";
+	else if (v == OU)
+		return "+";
+	else if (v == ET)
+		return ".";
+	else if (v == IMPLICATION)
+		return "=>";
+	else if (v == EQUIVALENCE)
+		return "<=>";
+	else if (v == GAUCHE)
+		return "(";
+	//else (v == DROITE)
+	return ")";
+}
 
+// Affiche la liste (DEBUG)
+void printList(liste_token l, char *name)
+{
+	printf("%s = ", name);
+	if (! l)
+	{
+		puts("null");
+		return;
+	}
+	while(l)
+	{
+		printf("%s", toString(l->valeur));
+		l = l->suivant;
+	}
+	putchar('\n');
+}
+
+// Transforme une chaîne de charactères en liste de token
+liste_token	string_to_token(const char *s)
+{
+	liste_token l = NULL, c = NULL;
+	enum type t;
+	enum valeur v;
+	while (*s)
+	{
+		t = OPERATEUR;
+		if (*s == ' ')		{ s++; continue; }
+		else if (*s == '0')	{ t = CONSTANTE; v = FAUX; }
+		else if (*s == '1')	{ t = CONSTANTE; v = VRAI; }
+		else if (*s == '+')	{ v = OU; }
+		else if (*s == '.')	{ v = ET; }
+		else if (*s == '(')	{ t = PARENTHESE; v = GAUCHE; }
+		else if (*s == ')')	{ t = PARENTHESE; v = DROITE; }
+		else
+		{
+			if (s[0] == '=' && s[1] && s[1] == '>')
+				v = IMPLICATION;
+			else if (s[0] == '<' && s[1] && s[1] == '=' && s[2] && s[2] == '>')
+			{
+				v = EQUIVALENCE;
+				s++;
+			}
+			else if (s[0] == 'N' && s[1] && s[1] == 'O' && s[2] && s[2] == 'N')
+			{
+				v = NON;
+				s++;
+			}
+			else EXIT_INVALIDE(); // non reconnu
+			s++;
+		}
+		if (! l)
+		{
+			l = new_liste_token(t, v);
+			c = l; // pointeur sur l'élément actuel
+		}
+		else ENQUEUE(c, t, v);
+		s++;
+	}
+	if (! l) EXIT_INVALIDE(); // expression vide
+	return l;
+}
+
+// Vérifie si l'expression est valide
+int	est_valide(liste_token l)
+{
+	// expr=non*c(ε+op expr)
+	while (l && l->valeur == NON)
+		l = l->suivant;
+	if (! l || l->type == OPERATEUR || l->type == PARENTHESE)
+		return 0;
+	l = l->suivant;
+	if (! l)
+		return 1;
+	else if (l->type == CONSTANTE || l->valeur == NON)
+		return 0;
+	return est_valide(l->suivant);
+}
+
+// Transforme une liste de tokens en arbre
 arbre_token	liste_token_to_arbre_token(liste_token l)
 {
 	if (! l)
@@ -238,69 +280,31 @@ int	arbre_to_int(arbre_token at)
 	return resoudre(arbre_to_int(at->gauche), arbre_to_int(at->droite), at->valeur);
 }
 
-const char*	getString(enum valeur v)
-{
-	if (v == FAUX)
-		return "0";
-	else if (v == VRAI)
-		return "1";
-	else if (v == NON)
-		return "NON";
-	else if (v == OU)
-		return "+";
-	else if (v == ET)
-		return ".";
-	else if (v == IMPLICATION)
-		return "=>";
-	else if (v == EQUIVALENCE)
-		return "<=>";
-	else if (v == GAUCHE)
-		return "(";
-	//else (v == DROITE)
-	return ")";
-}
-
-void printList(liste_token l, char *name)
-{
-	if (! l) return;
-	printf("%s = ", name);
-	while(l)
-	{
-		printf("%s", getString(l->valeur));
-		l = l->suivant;
-	}
-	putchar('\n');
-}
-
-// Parcours prefixe (debug)
+// Parcours prefixe (DEBUG)
 void	prefixe(arbre_token at, unsigned int p)
 {
 	if (! at)
 		return;
 	for (unsigned int i = 0; i < p; i++)
 		printf("  ");
-	printf("%s\n", getString(at->valeur));
+	printf("%s\n", toString(at->valeur));
 	prefixe(at->gauche, ++p);
 	prefixe(at->droite, p);
-}
-
-// Test d'une expression booléenne (debug)
-void	test_expression(const char *s)
-{
-	if (! s)
-		return;
-	liste_token l = string_to_token(s);
-	arbre_token at = liste_token_to_arbre_token(l);
-	prefixe(at, 0);
-	//printf((arbre_to_int(at)) ? "VRAI" : "FAUX");
-	//putchar('\n');
-	destroy_arbre_token(at);
 }
 
 int	main(int argc, char **argv)
 {
 	if (argc != 2)
 		EXIT_INVALIDE();
-	test_expression(argv[1]);
+	liste_token l = string_to_token(argv[1]);
+	
+	int v = est_valide(l);
+	puts((v) ? "VRAI" : "FAUX");
+	//if (! v) EXIT_INVALIDE();
+	
+	/*arbre_token a = liste_token_to_arbre_token(l);
+	prefixe(a, 0);
+	puts((arbre_to_int(a)) ? "VRAI" : "FAUX");
+	destroy_arbre_token(a);*/
 	return 0;
 }
